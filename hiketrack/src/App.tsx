5fedaf84ea051d1b5038ca25e31b1c866fb5be40
petarpopovic-web <petarpopovicdrugosedam@gmail.com>
2025-10-14 16:45:36 +0200

    App Layout with Navbar & Footer

--- hiketrack/src/App.tsx
+++ hiketrack/src/App.tsx
@@ -5,16 +5,20 @@ import Tracks from './pages/Tracks';
 import Track from './pages/Track';
 import Weather from './pages/Weather';
 import NotFound from './pages/NotFound';
+import Layout from './layout/Layout';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/tracks' element={<Tracks />} />
-          <Route path='/tracks/:id' element={<Track />} />
-          <Route path='/weather' element={<Weather />} />
+          <Route element={<Layout />}>
+            <Route path='/' element={<Home />} />
+            <Route path='/tracks' element={<Tracks />} />
+            <Route path='/tracks/:id' element={<Track />} />
+            <Route path='/weather' element={<Weather />} />
+          </Route>
+
           <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
